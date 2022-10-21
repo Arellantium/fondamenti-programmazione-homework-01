8622fa2e70a36616ce8519417f2908c7d84feea9
Alessandro Arellano <alessandroarellanoaltuna@gmail.com>
2022-10-22 01:05:19 +0200

    salvato con errori

--- program01.py
+++ program01.py
@@ -48,7 +48,7 @@ def ex1(int_seq, subtotal):
                 result += 1
                 while indice_lista + 1 < len(numbers) and numbers[indice_lista+1] == 0:
                     result += 1
-                    indice_lista += 1
+                    indice_lista += 1 sergio
 
                 numbers.pop(0)
                 break  
